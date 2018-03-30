87cfef3074bd17c82ad4df29cfb9c65426433e60
ilvitous <andreav@pridegroup.us>
2018-03-30 18:36:21 +0000

    test for digital ocean 2

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,32 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App.vue';
+import Dashboard from './components/Dashboard.vue';
 import Home from './components/Home.vue';
-
+import Register from './components/Register.vue';
+import Login from './components/Login.vue';
 Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+axios.defaults.baseURL = 'http://localhost:8000/api';
 
 const router = new VueRouter({
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home
-        },
-    ]
+    routes: [{
+        path: '/',
+        name: 'home',
+        component: Home
+    },{
+        path: '/register',
+        name: 'register',
+        component: Register
+    },{
+        path: '/login',
+        name: 'login',
+        component: Login
+    }]
 });
 
-new Vue({
-    el: '#app',
-    router: router,
-    render: app => app(App)
-});
+Vue.router = router
+App.router = Vue.router
+new Vue(App).$mount('#app');
